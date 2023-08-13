d54380e820cf50e2e122c0566ef268d4a6e865a4
Kaue Varela <kauevarella@hotmail.com>
2023-08-13 01:22:43 -0300

    Project Created

---
+++ src/main/java/com/kauevarela/mongodb_workshop/MongodbWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.kauevarela.mongodb_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kauevarela/mongodb_workshop/MongodbWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauevarela.mongodb_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
