ce09e26df14d88ccfa2e5f1e92f24af337e86273
Kaue Varela <kauevarella@hotmail.com>
2023-08-13 21:13:25 -0300

    Adding comments to posts

--- src/main/java/com/kauevarela/mongodb_workshop/config/Instantiation.java
+++ src/main/java/com/kauevarela/mongodb_workshop/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.kauevarela.mongodb_workshop.domain.Post;
 import com.kauevarela.mongodb_workshop.domain.User;
 import com.kauevarela.mongodb_workshop.dto.AuthorDTO;
+import com.kauevarela.mongodb_workshop.dto.CommentDTO;
 import com.kauevarela.mongodb_workshop.repository.PostRepository;
 import com.kauevarela.mongodb_workshop.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(kaue));
 		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(kaue));
 	
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(caio));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(johnson));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(caio));
+		
+		p1.getComments().addAll(Arrays.asList(c1, c2));
+		p2.getComments().add(c3);
+		
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
 		kaue.getPosts().addAll(Arrays.asList(p1, p2));
