bc827d50a220e48fb4a37d7085f52f10020c59cc
Kaue Varela <kauevarella@hotmail.com>
2023-08-13 21:25:20 -0300

    Simple search with query methods

--- src/main/java/com/kauevarela/mongodb_workshop/services/PostService.java
+++ src/main/java/com/kauevarela/mongodb_workshop/services/PostService.java
@@ -1,12 +1,12 @@
 package com.kauevarela.mongodb_workshop.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kauevarela.mongodb_workshop.domain.Post;
-import com.kauevarela.mongodb_workshop.domain.User;
 import com.kauevarela.mongodb_workshop.repository.PostRepository;
 import com.kauevarela.mongodb_workshop.services.exception.ObjectNotFoundException;
 
@@ -19,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
